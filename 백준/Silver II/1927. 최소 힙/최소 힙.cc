ce62395015645681584aaabee1b509e6194ca3ae
duguseo1213 <159924191+duguseo1213@users.noreply.github.com>
2024-02-14 22:14:04 +0900

    [Silver II] Title: 최소 힙, Time: 20 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver II/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#define INF 987654321
+
+
+
+using namespace std;
+
+struct edge{
+	int a;
+};
+
+struct cmp {
+	bool operator()(edge a,edge b)
+	{
+		return a.a > b.a;
+	}
+};
+
+
+priority_queue<edge, vector<edge>, cmp> pq;
+
+
+int main()
+{
+	int N;
+	cin >> N;
+	int temp;
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d", &temp);
+
+		if (temp == 0)
+		{
+			if (!pq.empty())
+			{
+				printf("%d\n", pq.top());
+				pq.pop();
+			}
+			else
+			{
+				printf("0\n");
+			}
+		}
+		else
+		{
+			pq.push({ temp });
+		}
+	}
+
+
+
+}
+
+
+
