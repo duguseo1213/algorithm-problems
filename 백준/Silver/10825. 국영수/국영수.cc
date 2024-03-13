1a0c56cad9e2b9ca494dcc8ad20169afe005decb
duguseo1213 <159924191+duguseo1213@users.noreply.github.com>
2024-03-13 20:36:11 +0900

    [Silver IV] Title: 국영수, Time: 144 ms, Memory: 6716 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.cc
+++ 백준/Silver/10825. 국영수/국영수.cc
@@ -1,52 +1,134 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <string>
+#include <set>
 #include <vector>
-#include <algorithm>
-#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <string>
 using namespace std;
 
-int N;
-
-struct student {
+struct Student {
+	int language;
+	int english;
+	int math;
 	string name;
-	int a;
-	int b;
-	int c;
 };
-student qqq[100000];
-bool cmp(student a, student b)
+
+Student heap[100010];
+int hn;
+
+bool cmp(Student a, Student b)
 {
-	if (a.a == b.a)
+	if (a.language == b.language)
 	{
-		if (a.b == b.b)
+		if (a.english == b.english)
 		{
-			if (a.c == b.c)
+			if (a.math == b.math)
 			{
-				return a.name < b.name;
+				return a.name > b.name;
 			}
-			return a.c > b.c;
+			return a.math < b.math;
 		}
-		return a.b < b.b;
+
+		return a.english > b.english;
+
 	}
 
-	return a.a > b.a;
+	return a.language < b.language; //뒤에께 우선순위 크면 true 반환
 }
+
+void push(Student a)
+{
+	hn++;
+
+	heap[hn] = a;
+	Student temp;
+
+	for (int i = hn; i > 1; i = i / 2)
+	{
+		if (cmp(heap[i], heap[i / 2])) break;
+
+		temp = heap[i];
+		heap[i] = heap[i / 2];
+		heap[i / 2] = temp;
+		
+
+
+	}
+
+
+}
+
+
+Student pop()
+{
+	Student worst;
+	worst.language = -10000;
+
+	Student ret = heap[1];
+	heap[1] = heap[hn];
+	heap[hn] = worst;
+
+	hn--;
+
+	Student temp;
+	for (int i = 1; i * 2 <= hn;)
+	{
+		if (!cmp(heap[i], heap[i * 2]) && !cmp(heap[i], heap[i * 2 + 1]))
+		{
+			break;
+		}
+		else if (cmp(heap[i * 2+1], heap[i * 2]))
+		{
+			temp = heap[i * 2];
+			heap[i * 2] = heap[i];
+			heap[i] = temp;
+
+			i = i * 2;
+		}
+		else
+		{
+			temp = heap[i * 2+1];
+			heap[i * 2+1] = heap[i];
+			heap[i] = temp;
+
+			i = i * 2+1;
+		}
+	}
+
+	return ret;
+
+
+
+
+
+}
+
+
 
 
 int main()
 {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int N;
 	cin >> N;
 	for (int i = 0; i < N; i++)
 	{
-		cin >> qqq[i].name >> qqq[i].a >> qqq[i].b >> qqq[i].c;
+		string temp;
+		Student s;
+		cin >> s.name;
+		cin >> s.language >> s.english >> s.math;
+
+
+		push(s);
 
 	}
 
-	sort(qqq, qqq + N, cmp);
 	for (int i = 0; i < N; i++)
 	{
-		cout << qqq[i].name << "\n";
-
+		cout << pop().name << "\n";
 	}
+	
 }
