a44a202feb7d88ca13902936ef2e6226d6b9c2a1
duguseo1213 <159924191+duguseo1213@users.noreply.github.com>
2024-03-17 23:55:15 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 236 ms, Memory: 8176 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+vector<int> ans;
+
+int N;
+int main()
+{
+	scanf("%d", &N);
+	int num;
+	scanf("%d", &num);
+
+	ans.push_back(num);
+
+	for (int k = 1; k < N; k++)
+	{
+		scanf("%d", &num);
+
+		if (num > ans.back())
+		{
+			ans.push_back(num);
+		}
+		else
+		{
+
+			int i = 0;
+			int j = ans.size() - 1;
+			int index=-1;
+			int flag = 0;
+			while (1)
+			{
+				if (i > j) break;
+
+				int mid = (i + j) / 2;
+				if (ans[mid] == num)
+				{
+					flag = 1;
+					break;
+				}
+				else if (ans[mid] < num)
+				{
+					index = mid;
+					i = mid + 1;
+					
+				}		
+				else
+				{
+					j = mid - 1;
+				}
+
+			}
+			if (flag == 0)
+			{
+				ans[index + 1] = num;
+			}
+			else
+			{
+
+			}
+
+		}
+		
+
+	}
+	printf("%d", ans.size());
+
+
+}
