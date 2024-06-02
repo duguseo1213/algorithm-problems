9c54de01d2c3ad57d99e79ad0feadf0a28f2336f
duguseo1213 <159924191+duguseo1213@users.noreply.github.com>
2024-06-02 09:52:16 +0900

    [Silver I] Title: 1, 2, 3 더하기 7, Time: 20 ms, Memory: 9992 KB -BaekjoonHub

---
+++ 백준/Silver/15992. 1， 2， 3 더하기 7/1， 2， 3 더하기 7.cc
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define P printf
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+long long dp[1010][1010]; //개수,값
+
+int N, M;
+int T;
+int main() 
+{
+	scanf("%d", &T);
+	//scanf("%d %d", &N, &M);
+
+	dp[1][1] = 1;
+
+	dp[1][2] = 1;
+	dp[2][2] = 1;
+
+	dp[1][3] = 1;
+	dp[2][3] = 2;
+	dp[3][3] = 1;
+
+
+	for (int i = 4; i <= 1000; i++)
+	{
+		for (int j = 1; j <= 1000; j++)
+		{
+			dp[j][i] += dp[j - 1][i - 1] % 1000000009;
+			dp[j][i] += dp[j - 1][i - 2] % 1000000009;
+			dp[j][i] += dp[j - 1][i - 3] % 1000000009;
+
+		}
+	}
+
+	for (int i = 0; i < T; i++)
+	{
+		int a, b;
+		scanf("%d %d", &a, &b);
+		printf("%lld\n", dp[b][a] % 1000000009);
+
+	}
+	
+	
+}
