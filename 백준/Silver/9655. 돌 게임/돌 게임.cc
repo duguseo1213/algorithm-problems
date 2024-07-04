5a1184d4857fb6e220247bbcc209767faf6ceb3e
duguseo1213 <159924191+duguseo1213@users.noreply.github.com>
2024-07-04 15:59:18 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define P printf
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <string>
+using namespace std;
+
+int N;
+int main(){
+    scanf("%d", &N);
+
+    if (N % 2 == 0) {
+        printf("CY");
+    }
+    else
+    {
+        printf("SK");
+    }
+    
+    
+
+}
