cb63aadb73aa03ea059cead3c49b9ecd837d4f8e
duguseo1213 <159924191+duguseo1213@users.noreply.github.com>
2024-08-09 21:20:55 +0900

    [level 2] Title: 점 찍기, Time: 11.58 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+long long solution(int k, int d) {
+    long long answer = 0;
+    
+    for(long long i=0;i<=d;i+=k){
+        
+        long long temp=sqrt((long long)d*d-(long long)i*i);
+        
+        answer+=temp/k+1;
+        
+    }
+    
+    
+    return answer;
+}
