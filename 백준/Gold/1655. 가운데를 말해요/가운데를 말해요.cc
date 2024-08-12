f77d6be819fa48ae6fd99f84acf4d91974c458c9
duguseo1213 <159924191+duguseo1213@users.noreply.github.com>
2024-08-13 00:18:30 +0900

    [Gold II] Title: 가운데를 말해요, Time: 28 ms, Memory: 2712 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.cc
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.cc
@@ -1,79 +1,57 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <cstring>
+#include <cmath>
+#include <map>
 #include <set>
-#include <vector>
 #include <queue>
 
 using namespace std;
 
-priority_queue<int, vector<int>> pq1; //max heap
-priority_queue<int, vector<int>,greater<int>> pq2;//min heap
+priority_queue<int, vector<int>> maxheap;
+priority_queue<int, vector<int>,greater<int>> minheap;
 
-//현재 중앙값보다 크면 minheap // 작으면 maxheap으로 넣어주기
 
-int main()
-{
-	int N;
-	cin >> N;
 
-	int center = 0;
+int N;
 
-	int temp;
-	scanf("%d", &temp);
-	pq1.push(temp);
+int main() {
 
-	center = temp;
-	
-	cout << center<<endl;
+    scanf("%d", &N);
 
-	for (int i = 1; i < N; i++)
-	{
-		scanf("%d", &temp);
+    minheap.push(21e8);
+    maxheap.push(-21e8);
 
-		if (pq1.size() == pq2.size())
-		{
-			if (temp > center)
-			{
-				pq2.push(temp);
-				center = pq2.top();
-			}
-			else
-			{
-				pq1.push(temp);
-				center = pq1.top();
-			}
-		}
-		else if (pq1.size() > pq2.size())
-		{
-			if (temp > center)
-			{
-				pq2.push(temp);
-			}
-			else
-			{
-				pq2.push(pq1.top());
-				pq1.pop();
-				pq1.push(temp);
-			}
-			center = pq1.top();
-		}
-		else
-		{
-			if (temp > center)
-			{
-				pq1.push(pq2.top());
-				pq2.pop();
-				pq2.push(temp);
-			}
-			else
-			{
-				pq1.push(temp);
-			}
-			center = pq1.top();
-		}
-		
+    for (int i = 0; i < N; i++) {
 
-		printf("%d\n", center);
-	
-	}
+        int num;
+        scanf("%d", &num);
+
+        if (num > minheap.top()) {
+            minheap.push(num);
+        }
+        else {
+            maxheap.push(num);
+        }
+
+        if (minheap.size() > maxheap.size()) {
+            int temp = minheap.top();
+            minheap.pop();
+            maxheap.push(temp);
+        }
+        else if(minheap.size()+1<maxheap.size()){
+            int temp = maxheap.top();
+            maxheap.pop();
+            minheap.push(temp);
+        }
+
+        printf("%d\n", maxheap.top());
+
+
+
+
+    }
+
+    
+
 }
