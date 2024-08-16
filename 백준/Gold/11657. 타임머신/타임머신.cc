192e560f58ad69b46ea8a129eb38c0fd7a464967
duguseo1213 <159924191+duguseo1213@users.noreply.github.com>
2024-08-16 12:06:47 +0900

    [Gold IV] Title: 타임머신, Time: 8 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.cc
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -1,96 +1,87 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <cstring>//memset
 #include <string>
-#include <algorithm>
 #include <vector>
-#include <cstdlib>
-#include <queue>
-#include <cstring>
-#include <climits>
+
 using namespace std;
 
 struct edge {
 	int start;
 	int end;
-	int weight;
+	int cost;
 };
 
 vector<edge> edges;
-long long dist[510];
+
+long long dist[501];
+
 int N, M;
 
-void func()
-{
-	for (int i = 1; i <=N-1; i++)
-	{
-		for (int j = 0; j < edges.size(); j++)
-		{
-			if (dist[edges[j].start] == 21e8) continue;
+int main() {
+	
+	scanf("%d %d", &N, &M);
 
-			if (dist[edges[j].start] + edges[j].weight < dist[edges[j].end])
-			{
-				
-				dist[edges[j].end] = dist[edges[j].start] + edges[j].weight;
-			}
-			
-
-
-		}
-		
-	}
-}
-
-
-int main()
-{
-	cin >> N >> M;
-
-	int state = 0;
-	edge temp;
-	for (int i = 1; i <= N; i++)
-	{
+	for (int i = 0; i <= 501; i++) {
 		dist[i] = 21e8;
 	}
+
+	for (int i = 0; i < M; i++) {
+		edge temp;
+
+		scanf("%d %d %d", &temp.start, &temp.end, &temp.cost);
+		
+		edges.push_back(temp);
+		
+	}
+
 	dist[1] = 0;
 
-	for (int i = 0; i < M; i++)
-	{
-		scanf("%d %d %d", &temp.start, &temp.end, &temp.weight);
+	for (int i = 0; i < N - 1; i++) {
 
-		edges.push_back(temp);
+		for (int j = 0; j < edges.size(); j++) {
+
+			if(dist[edges[j].start] == 21e8) {
+				continue;
+			}
+
+			if(dist[edges[j].start] + edges[j].cost < dist[edges[j].end]) {
+
+				dist[edges[j].end] = dist[edges[j].start] + edges[j].cost;
+
+			}
+
+		}
 
 	}
+
+	for (int j = 0; j < edges.size(); j++) {
+
+		if (dist[edges[j].start] == 21e8) {
+			continue;
+		}
+
+		if (dist[edges[j].start] + edges[j].cost < dist[edges[j].end]) {
+
+			dist[edges[j].end] = dist[edges[j].start] + edges[j].cost;
+			printf("-1");
+			return 0;
+		}
+
+	}
+
+	for (int i = 2; i <= N; i++) {
+		if (dist[i] == 21e8) {
+			printf("-1\n");
+		}
+		else {
+			printf("%d\n", dist[i]);
+		}
+	}
+
 	
-	func();
-	for (int j = 0; j < edges.size(); j++)
-	{
-		if (dist[edges[j].start] == 21e8) continue;
 
-		if (dist[edges[j].start] + edges[j].weight < dist[edges[j].end])
-		{
-			state = 1;
-			break;
-		}
 
-	}
 
-	if (state == 1)
-	{
-		cout << -1;
-	}
-	else
-	{
-		for (int i = 2; i <= N; i++)
-		{
-			if (dist[i] == 21e8)
-			{
-				printf("-1\n");
-			}
-			else
-			{
-				printf("%d\n", dist[i]);
-			}
-		}
-	}
 
 }
