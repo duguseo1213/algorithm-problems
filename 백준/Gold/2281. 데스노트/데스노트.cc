42f771799febfebdc93fbd9c8351d872fa8c4f2c
duguseo1213 <159924191+duguseo1213@users.noreply.github.com>
2024-10-01 12:04:44 +0900

    [Gold III] Title: 데스노트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2281. 데스노트/데스노트.cc
+++ 백준/Gold/2281. 데스노트/데스노트.cc
@@ -1,94 +1,50 @@
-#define _CRT_SECURE_NO_WARNINGS
+/*
+백준 골드4 데스노트
+사용하지 않는 칸의 제곱의 합이 최소가 되어야함.
+마지막 줄의 남는 칸은 계산하면 안됨.
+*/
 #include <iostream>
 #include <vector>
-#include <set>
 #include <algorithm>
-#include <cstring>
+#include <math.h>
+#include <climits>
 using namespace std;
-
-int dp[1001][1001];
-int N, M;
-
-int arr[1001];
-
-
-int func(int cnt,int line) {
-
-	
-
-	if (cnt == N-1) {
-		return 0;
-	}
-
-
-	if (dp[cnt][line] != 21e8) {
-		return dp[cnt][line];
-	}
-
-	int temp = M;
-
-	int i = cnt;
-
-	while (1) {
-
-		temp = temp - arr[i];
-
-		if (temp < 0) {
-			return dp[cnt][line];
-		}
-
-		
-
-		if (i == N) {
-			
-			return 0;
-
-		}
-		dp[cnt][line] = min(dp[cnt][line], func(i + 1, line + 1) + temp * temp);
-
-
-
-		
-		//if (temp <= 1) break;
-		temp--;
-
-		
-		i++;
-
-	}
-	//temp++;
-	//dp[cnt][line] = min(dp[cnt][line], temp * temp);
-	
-	//printf("%d %d %d\n", cnt, line,dp[cnt][line]);
-
-	return dp[cnt][line];
-
-
-}
-
-
+int n, m, now;
+vector<int> name;
+vector<int> DP; 
+// DP[i]는 i번째 이름을 처음으로 쓸 경우의 공백의 최솟값을 넣어줌. 다음 칸을 붙일지 안붙일지만 결정해주면 됨.
+int paper;
+int dp(int step);
 int main()
 {
-	
-	memset(dp, -1, sizeof(dp));
-
-	for (int i = 0; i < 1001; i++) {
-		for (int j = 0; j < 1001; j++) {
-			dp[i][j] = 21e8;
-		}
-	}
-
-
-	scanf("%d %d", &N, &M);
-
-	for (int i = 0; i < N; i++) {
-
-		scanf("%d", &arr[i]);
-
-	}
-
-	printf("%d", func(0, 0));
-
-
-
+    cin >> n >> m;
+    name.resize(n+1, 0), DP.resize(n+1,INT_MAX);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> name[i];
+    }
+    DP[n - 1] = 0; //마지막 줄은 공백을 계산하지 않아도 되기 때문에 0이다.
+    cout<<dp(0)<<endl;
+    
+    return 0;
+}
+int dp(int step) { //step은 현재 내가 몇번째 이름까지 검사햇는지 체크하는 것.
+    //이름이 너비를 넘지 않으면 일단 재귀를 넘겨보자.
+    //현재상태에서 잔여칸은  잔여-name[i]가 될거같음.
+    if (DP[step] < INT_MAX)
+    {
+        return DP[step];
+    }
+    
+    int Paper = m - name[step]; //잔여를 계속 step마다 초기화함.
+    for (int i = step + 1; i <= n && Paper >= 0; i++) { //현재 step 다음 거붙어 이어붙이는 거죵
+        if (i == n) { //끝까지 돌았다는 뜻.
+            DP[step] = 0;
+            break;
+        }
+        DP[step] = min(DP[step], Paper * Paper + dp(i));
+        Paper -= name[i] + 1; //종이를 이어붙인 경우 ,, 항상 이름+공백 한칸이 들어가기 때문에 뺴준다.
+        //현재 step의 dp값은 이어붙일때와 이어붙이지 않을 때 중 최소값이 들어가게 될것이다.
+    }
+    return DP[step];
 }
