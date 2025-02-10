b76e4bb2fe5de12157fe86565f32976b34f275fd
duguseo1213 <159924191+duguseo1213@users.noreply.github.com>
2025-02-10 21:45:22 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 408 ms, Memory: 8928 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.c
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int b[1000000];
+
+int a[1000000];
+
+int N;
+
+void merge(int start,int end) {
+
+	int mid, i, j, k;
+
+	mid = (start + end) / 2;
+
+	i = start;
+	j = mid + 1;
+	k = 0;
+
+	while (i <= mid && j <= end) {
+
+		if (a[i] <= a[j]) b[k++] = a[i++];
+		else b[k++] = a[j++];
+	}
+
+	while(i<=mid)  b[k++] = a[i++];
+	while (j <= end)  b[k++] = a[j++];
+
+	for (i = start; i <= end; i++) {
+		a[i] = b[i - start];
+	}
+
+
+}
+
+
+void sort(int start,int end) {
+
+	if (start >= end) return;
+
+	int mid = (start + end) >> 1;
+
+	sort(start, mid);
+	sort(mid + 1, end);
+	merge(start, end);
+
+
+}
+
+
+int main() {
+
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	sort(0, N-1);
+
+	for (int i = 0; i < N; i++) {
+		printf("%d\n", a[i]);
+	}
+	
+	
+
+}
