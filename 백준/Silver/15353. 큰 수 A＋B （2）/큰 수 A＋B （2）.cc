9ee44053113f2f51a24820dade6b197c84f5fb05
duguseo1213 <159924191+duguseo1213@users.noreply.github.com>
2025-03-23 18:08:55 +0900

    [Silver III] Title: 큰 수 A+B (2), Time: 0 ms, Memory: 1336 KB -BaekjoonHub

--- 백준/Silver/15353. 큰 수 A＋B （2）/큰 수 A＋B （2）.cc
+++ 백준/Silver/15353. 큰 수 A＋B （2）/큰 수 A＋B （2）.cc
@@ -1,83 +1,121 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-char tb[1000000];
+#define MAX (10010)
 
-char ta[1000000];
+typedef long long int ll;
 
-char a[1000000];
-char b[1000000];
+#define NOTATION (100000000000000000)
+#define NOT_COUNT (17) // ↑ 0의 개수
 
-char sum[100000];
-
-
-int N;
-
-
-int main() {
-
-	scanf("%s %s", ta, tb);
-	int i = 0;
-	int alen=0;
-	int blen=0;
-
-	while (1) {
-		if (ta[alen] == '\0') break;
-		alen++;
-
-	}
-	while (1) {
-		if (tb[blen] == '\0') break;
-		blen++;
-	}
-
-	//printf("%d %d\n", alen, blen);
-
-	for (int i = 0; i < alen; i++) {
-		a[i] = ta[alen - 1 - i];
-	}
-
-	for (int i = 0; i < blen; i++) {
-		b[i] = tb[blen - 1 - i];
-	}
-
-	
+typedef struct st
+{
+	ll num[MAX]; // <-  // long 연산 때문에 MAX 보다 작은 값을 사용해도 된다.
 	int len;
-	if (blen > alen) {
-		len = blen;
-	}
-	else {
-		len = alen;
+}BIG_NUMBER;
+
+char A[MAX];
+char B[MAX];
+char ANSWER[MAX];
+
+BIG_NUMBER bigPlus(BIG_NUMBER& a, BIG_NUMBER& b)
+{
+	BIG_NUMBER result = { 0 };
+	int lenA, lenB, len;
+	ll temp[MAX] = { 0 };
+
+	lenA = a.len;
+	lenB = b.len;
+	len = (lenA > lenB) ? lenA : lenB;
+
+	for (int i = 0; i < lenA; i++) temp[i] += a.num[i];
+	for (int i = 0; i < lenB; i++) temp[i] += b.num[i];
+
+	for (int i = 0; i < len; i++)
+	{
+		temp[i + 1] += (temp[i] / NOTATION);
+		temp[i] %= NOTATION;
 	}
 
-	for (int i = alen; i < len; i++) {
-		a[i] = '0';
+	len++; // 자릿수가 오른 경우 체크
+	while (len && temp[len] == 0) len--;
+	len++;
+
+	for (int i = 0; i < len; i++) result.num[i] = temp[i];
+
+	result.len = len;
+
+	return result;
+}
+
+void makeNumber(BIG_NUMBER& big, char number[MAX])
+{
+	char reverse[MAX] = { 0 };
+	int len, nLen;
+
+	for (len = 0; number[len]; len++);
+
+	for (int i = 0; i < len; i++) reverse[i] = number[len - i - 1] - '0';
+
+	nLen = 0;
+	for (int i = 0; i < len; i += NOT_COUNT)
+	{
+		ll tmp = 0;
+		for (int k = i + NOT_COUNT - 1; k >= i; k--) tmp = tmp * 10 + reverse[k];
+
+		big.num[nLen++] = tmp;
 	}
-	for (int i = blen; i < len; i++) {
-		b[i] = '0';
-	}
 
-	for (int i = 0; i < len; i++) {
+	big.len = nLen;
+}
 
-		sum[i] = a[i] + b[i]-'0'+sum[i];
+void changeIntToChar(BIG_NUMBER& big, char answer[MAX])
+{
+	char temp[MAX] = { 0 };
 
-		if (sum[i] > '9') {
-			sum[i] -= 10;
-			sum[i + 1] = 1;
+	int len = big.len;
+
+	int count = 0;
+	for (int i = 0; i < len; i++)
+	{
+		for (int k = 0; k < NOT_COUNT; k++)
+		{
+			temp[count++] = (big.num[i] % 10);
+			big.num[i] /= 10;
 		}
-
-
 	}
-	
-	for (int i = 0; i < len; i++) {
-		b[i] = sum[len - 1 - i];
-	}
-	if (sum[len] == 1) {
-		printf("1%s", b);
-	}
-	else {
 
+	while (count && temp[count] == 0) count--;
+	count++;
 
-		printf("%s", b);
+	temp[count] = 0;
+
+	int half = count / 2;
+	for (int i = 0; i < half; i++)
+	{
+		char tmp = temp[i];
+		temp[i] = temp[count - 1 - i];
+		temp[count - 1 - i] = tmp;
 	}
+
+	for (int i = 0; i < count; i++) answer[i] = temp[i] + '0';
+
+	answer[count] = 0;
+}
+
+int main()
+{
+	scanf("%s %s", A, B);
+
+	BIG_NUMBER a, b, result;
+
+	makeNumber(a, A);
+	makeNumber(b, B);
+
+	result = bigPlus(a, b);
+
+	changeIntToChar(result, ANSWER);
+
+	printf("%s\n", ANSWER);
+
+	return 0;
 }
