fe4d1b574f2caa39159a9c2353dd11ac0fb1a8e4
duguseo1213 <159924191+duguseo1213@users.noreply.github.com>
2025-03-23 15:51:31 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 404 ms, Memory: 8928 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.c
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.c
@@ -7,34 +7,48 @@ int a[1000000];
 
 int N;
 
-void merge(int start,int end) {
+void merge(int start, int end) {
 
-	int mid, i, j, k;
+	int i, j, k, mid;
 
 	mid = (start + end) / 2;
 
 	i = start;
 	j = mid + 1;
-	k = 0;
+	k = start;
+
 
 	while (i <= mid && j <= end) {
 
-		if (a[i] <= a[j]) b[k++] = a[i++];
-		else b[k++] = a[j++];
+		if (a[i] < a[j]) {
+			b[k++] = a[i++];
+		}
+		else {
+			b[k++] = a[j++];
+		}
+
 	}
 
-	while(i<=mid)  b[k++] = a[i++];
-	while (j <= end)  b[k++] = a[j++];
+	while (i <= mid) {
+		b[k++] = a[i++];
+	}
+
+	while (j <= end) {
+		b[k++] = a[j++];
+	}
 
 	for (i = start; i <= end; i++) {
-		a[i] = b[i - start];
+		a[i] = b[i];
 	}
+
+
 
 
 }
 
 
-void sort(int start,int end) {
+
+void sort(int start, int end) {
 
 	if (start >= end) return;
 
@@ -42,6 +56,7 @@ void sort(int start,int end) {
 
 	sort(start, mid);
 	sort(mid + 1, end);
+
 	merge(start, end);
 
 
@@ -56,12 +71,12 @@ int main() {
 		scanf("%d", &a[i]);
 	}
 
-	sort(0, N-1);
+	sort(0, N - 1);
 
 	for (int i = 0; i < N; i++) {
 		printf("%d\n", a[i]);
 	}
-	
-	
+
+
 
 }
