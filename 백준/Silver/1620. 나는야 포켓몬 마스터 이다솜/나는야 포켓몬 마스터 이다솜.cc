c43b4e64e5088f5846f84b48240c577606942d7d
duguseo1213 <159924191+duguseo1213@users.noreply.github.com>
2025-04-06 17:51:41 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 88 ms, Memory: 11368 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,39 +1,120 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <set>
-#include <vector>
+#include <stdio.h>
 #include <queue>
-#include <map>
-#include <string>
+#include <vector>
+#include <cstring>
+#define MAX_VALUE 10007
+
+
+typedef unsigned long long ull;
+
 using namespace std;
 
-map<string, string> m1;
+int N, M;
+int pcnt = 0;
 
-map<string, string> m2;
+struct pokemon {
 
-int main()
+
+	int number;
+	pokemon* next;
+	char name[21];
+
+};
+
+pokemon pool[200000];
+pokemon HEAD[MAX_VALUE];
+
+char reverses[100001][21];
+
+ull hashfunc(const char* str) {
+
+	ull hash = 5381;
+
+	int c;
+
+	while (c = *str++) {
+
+		hash = (((hash << 5) + c) + hash) % MAX_VALUE;
+
+
+	}
+
+
+
+
+	return hash % MAX_VALUE;
+
+
+}
+
+int change(char* str)
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	string temp;
-	int N, M;
-	cin >> N >> M;
-	for (int i = 1; i <= N; i++)
+	int i, len;
+	int sum, mul;
+
+	sum = 0;
+	mul = 1;
+
+	for (i = 0; str[i]; i++);
+	len = i;
+
+	for (i = len - 1; i >= 0; i--)
 	{
-		cin >> temp;
-		m1[temp] = to_string(i);
-		m1[to_string(i)] = temp;
-		//cout << to_string(i);
+		sum += (str[i] - '0') * mul;
+		mul *= 10;
+	}
+
+	return sum;
+}
+
+int main(void)
+{
+	scanf("%d %d", &N, &M);
+	char str[21];
+	for (int i = 1; i <= N; i++) {
 		
-	}
-	
+		pcnt++;
+		pokemon* temp=&pool[pcnt];
+		scanf("%s", temp->name);
+		temp->number = pcnt;
+		ull hashnum = hashfunc(temp->name);
 
-	for (int j = 0; j < M; j++)
+		temp->next = HEAD[hashnum].next;
+		HEAD[hashnum].next = temp;
+
+		strcpy(reverses[pcnt], temp->name);
+
+	}
+
+	for (int i = 0; i < M; i++)
 	{
-		cin >> temp;
+		scanf("%s", str);
 
-		cout << m1[temp]<<"\n";
+		if ('0' < str[0] && str[0] <= '9')
+		{
+			int index = change(str);
+
+			printf("%s\n", reverses[index]); /* ARR에 순서대로 저장했기 때문에 그대로 출력 */
+		}
+		else
+		{
+			ull h = hashfunc(str);
+			pokemon* nd = HEAD[h].next;
+
+			while (nd)
+			{
+				if (!strcmp(nd->name, str))
+				{
+					printf("%d\n", nd->number);
+					break;
+				}
+
+				nd = nd->next;
+			}
+		}
 	}
+
+
 
 }
