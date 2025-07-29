2c474dea1463bd7197f48c2d8f9c55320c092497
duguseo1213 <159924191+duguseo1213@users.noreply.github.com>
2025-07-29 22:48:39 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,18 +1,38 @@
-#include<iostream>
+#define P
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+#include <cstring>
+
 using namespace std;
-int main()
-{
-    int dp[11] = {0, 1, 2, 4, 7};
-    for (int i = 5; i < 11; i++)
-    {
-        dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
-    }
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        int a;
-        cin >> a;
-        printf("%d\n", dp[a]);
-    }
+
+int dp[12];
+
+
+
+
+
+
+int main(){
+
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
+
+	for (int i = 4; i < 12; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+	}
+
+	int T;
+	scanf("%d", &T);
+
+	for (int i = 0; i < T; i++) {
+		int temp;
+		scanf("%d", &temp);
+		printf("%d\n", dp[temp]);
+	}
 }
